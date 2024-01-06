c187ad6449c7ff3bad06cb064f1ea6493f270b6c
Joe-Itax <josephitakala18@gmail.com>
2024-01-06 14:15:55 +0000

    feat: configuration of react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
+import Layout from "./components/layout/layout";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Result from "./pages/result";
+
 import "./App.css";
 
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate replace to="/login" />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/home",
+        element: <Home />,
+      },
+      {
+        path: "/result",
+        element: <Result />,
+      },
+    ],
+  },
+]);
 export default function App() {
-  return (
-    <main>
-      <h1 className="text-3xl font-bold underline">Winning Digits</h1>
-    </main>
-  );
+  return <RouterProvider router={routes} />;
 }
