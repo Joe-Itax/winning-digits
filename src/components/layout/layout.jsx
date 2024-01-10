dcfd7e3cb3cea81aba88a86f20ba5d83ecd6907b
Joe-Itax <josephitakala18@gmail.com>
2024-01-10 22:36:43 +0000

    feat: making the Login cmponent

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../footer/footer";
+// import Footer from "../footer/footer";
 
 function Layout() {
   return (
     <>
-      <main>
+      <main className="h-screen relative text-gray-100">
         <Outlet />
       </main>
 
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,32 @@
+import amicoSvg from "../assets/amico.svg";
+
 function Login() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Login</h1>
-    </div>
+    <>
+      <div className="bg-violet-950">
+        <h1 className="text-3xl text-white text-center font-bold uppercase py-4">
+          Winning Digits
+        </h1>
+      </div>
+      <section className="bg-[#371577] absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-1/2 flex justify-center items-start py-10 px-5 rounded-xl">
+        <div className="">
+          <img src={amicoSvg} alt="amico" className="w-full" />
+        </div>
+        <form className="">
+          <h2 className="text-center">Se Connecter</h2>
+          <div className="border relative rounded-lg border-gray-100">
+            <span className="absolute bg-[#371577] top-2/4 left-3 -translate-y-1/2">
+              Noms
+            </span>
+            <input
+              type="text"
+              className="outline-none bg-transparent h-full py-4 pl-3 pr-1"
+              required
+            />
+          </div>
+        </form>
+      </section>
+    </>
   );
 }
 export default Login;
